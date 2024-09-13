27a4b87336416d27b34927a9b4f23c63ef04fd9e
Luiz Fabiano de Souza <luizfabianods04@gmail.com>
2024-09-13 13:00:03 -0300

    Segundo projeto e builder de filme

---
+++ ScreenMatch/src/Principal.java
@@ -0,0 +1,12 @@
+public class Principal {
+    public static void main(String[] args) {
+
+        Filme meuFilme = new Filme.FilmeBuilder()
+                .nome("Filme")
+                .build();
+
+        
+    }
+
+
+}
